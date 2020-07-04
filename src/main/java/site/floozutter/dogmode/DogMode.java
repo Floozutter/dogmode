e865f89ce3a104894e48cf81f40ef85e72b7f7a6
Floozutter <floozutter@gmail.com>
2020-07-04 02:25:04 -0700

    Change style on examples

--- src/main/java/site/floozutter/dogmode/DogMode.java
+++ src/main/java/site/floozutter/dogmode/DogMode.java
@@ -18,63 +18,70 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod("dogmode")
 public class DogMode {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public DogMode() {
-        // Register the setup method for modloading
+        // Register modloading methods.
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+	// Examples.
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
-
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+		// Client only.
+        LOGGER.info(
+			"Got game settings {}",
+			event.getMinecraftSupplier().get().gameSettings
+		);
     }
-
     private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("dogmode", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        // Dispatch InterModComm to another mod.
+        InterModComms.sendTo(
+			"dogmode",
+			"helloworld",
+			() -> {
+				LOGGER.info("Hello world from the MDK");
+				return "Hello world";
+			}
+		);
     }
-
     private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
+		// Receive and process InterModComms from other mods.
+        LOGGER.info(
+			"Got IMC {}",
+			event.getIMCStream().
                 map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
+                collect(Collectors.toList())
+		);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+	// SubsribeEvent example to let the Event Bus discover methods to call.
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
+    // EventBusSubscriber example to automatically subscribe to events on the contained class.
+    // (This is subscribing to the MOD Event Bus for receiving Registry Events.)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
+            // (Register a new block here.)
             LOGGER.info("HELLO from Register Block");
         }
     }
