9b40d34b9e6715b9343c913e4ee4d33c8c4418ec
Floozutter <floozutter@gmail.com>
2020-07-05 05:03:57 -0700

    Fix filename case

---
+++ src/main/java/site/floozutter/dogmode/KeyBinds.java
@@ -0,0 +1,45 @@
+package site.floozutter.dogmode;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import java.util.ArrayList;
+
+
+public final class KeyBinds {
+	private static ArrayList<BoundRunnable> binds = new ArrayList<>();
+	
+	public static void add(KeyBinding binding, Runnable proc) {
+		binds.add(new BoundRunnable(binding, proc));
+	}
+	
+	public static void setup() {
+		binds.forEach(BoundRunnable::register);
+	}
+	
+	@SubscribeEvent
+	public static void onTick(TickEvent event) {
+		binds.forEach(BoundRunnable::run);
+	}
+	
+	private static class BoundRunnable {
+		private final KeyBinding binding;
+		private final Runnable proc;
+		
+		public BoundRunnable(KeyBinding binding, Runnable proc) {
+			this.binding = binding;
+			this.proc = proc;
+		}
+
+		public void register() {
+			ClientRegistry.registerKeyBinding(binding);
+		}
+		
+		public void run() {
+			if (binding.isPressed()) {
+				proc.run();
+			}
+		}
+	}
+}
