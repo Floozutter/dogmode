093f6fb5347f59395a7a23838c86a2ace3ee3235
Floozutter <floozutter@gmail.com>
2020-07-10 12:29:58 -0700

    Clean up the mod file

--- src/main/java/site/floozutter/dogmode/DogMode.java
+++ src/main/java/site/floozutter/dogmode/DogMode.java
@@ -4,24 +4,14 @@ import site.floozutter.dogmode.KeyBinds;
 import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.stream.Collectors;
 
 
 @Mod(DogMode.MODID)
@@ -32,73 +22,22 @@ public final class DogMode {
 
 	public DogMode() {
 		// Register modloading methods.
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
 
 		// Register to EVENT_BUS.
-		MinecraftForge.EVENT_BUS.register(this);
 		MinecraftForge.EVENT_BUS.register(KeyBinds.class);
 
 		// Test logging.
-		LOGGER.info("AAAAA!!!!!");
+		LOGGER.info("DogMode constructed!");
 	}
 
-	// Examples.
-	private void setup(final FMLCommonSetupEvent event) {
-		LOGGER.info("HELLO FROM PREINIT");
-		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-	}
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		// Client only.
-		LOGGER.info(
-			"Got game settings {}",
-			event.getMinecraftSupplier().get().gameSettings
-		);
-		
+	private void setupClient(final FMLClientSetupEvent event) {
 		KeyBinds.add(
-			new KeyBinding("owo", GLFW.GLFW_KEY_Z, "OWO"),
+			new KeyBinding("owo", GLFW.GLFW_KEY_Z, "DogMode"),
 			() -> LOGGER.info("press me harder uwu")
 		);
 		KeyBinds.setup();
-	}
-	private void enqueueIMC(final InterModEnqueueEvent event) {
-		// Dispatch InterModComm to another mod.
-		InterModComms.sendTo(
-			"dogmode",
-			"helloworld",
-			() -> {
-				LOGGER.info("Hello world from the MDK");
-				return "Hello world";
-			}
-		);
-	}
-	private void processIMC(final InterModProcessEvent event) {
-		// Receive and process InterModComms from other mods.
-		LOGGER.info(
-			"Got IMC {}",
-			event.getIMCStream().
-				map(m->m.getMessageSupplier().get()).
-				collect(Collectors.toList())
-		);
-	}
-
-	// SubsribeEvent example to let the Event Bus discover methods to call.
-	@SubscribeEvent
-	public void onServerStarting(FMLServerStartingEvent event) {
-		// do something when the server starts
-		LOGGER.info("HELLO from server starting");
-	}
-
-	// EventBusSubscriber example to automatically subscribe to events on the contained class.
-	// (This is subscribing to the MOD Event Bus for receiving Registry Events.)
-	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-			// (Register a new block here.)
-			LOGGER.info("HELLO from Register Block");
-		}
+		
+		LOGGER.info("DogMode activated!");
 	}
 }
